30f142038da4f46b9431b5c44de5a1095bf21261
Alex Vinnov <alex@vinnov.ru>
2024-04-11 14:26:15 +0300

    ДЗ-2.2

--- src/test/java/HellowWorld.java
+++ src/test/java/HellowWorld.java
@@ -1,16 +1,22 @@
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 
 public class HellowWorld {
 
     @Test
     public void helloWorld() {
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
+        Response response = get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
     }
@@ -23,6 +29,34 @@ public class HellowWorld {
                 .jsonPath();
 
         response.prettyPrint();
+
+        String timestamp1 = response.get("messages[2].timestamp");
+        System.out.println("Timestamp: " + timestamp1);
+
+        List<Map<String, String>> messages = response.getList("messages");
+        for (Map<String, String> message : messages) {
+            String timestamp = message.get("timestamp");
+            System.out.println("Timestamp: " + timestamp);
+        }
+    }
+
+    @Test
+    public void helloWorld3() {
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        response.prettyPrint();
+
+        Headers mmm = response.getHeaders();
+        System.out.println("Заголовки - " + mmm);
+
+        String mmm1 = response.getHeader("Location");
+        System.out.println("Редирект на  - " + mmm1);
     }
 
 }
