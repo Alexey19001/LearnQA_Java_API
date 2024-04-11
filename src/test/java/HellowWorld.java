98c8b64d55ee07700b6770a20bf465ebdf032205
Alex Vinnov <alex@vinnov.ru>
2024-04-11 10:55:51 +0300

    ДЗ-2.1

--- src/test/java/HellowWorld.java
+++ src/test/java/HellowWorld.java
@@ -1,14 +1,28 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class HellowWorld {
 
     @Test
-    public void helloWorld(){
+    public void helloWorld() {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
     }
+
+    @Test
+    public void helloWorld2() {
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        response.prettyPrint();
+    }
+
 }
